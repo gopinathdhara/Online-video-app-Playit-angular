ce596df5b7cb314f55d3d5f1841812cf78361eb2
gopi12 <gopi1.dhara@gmail.com>
2021-06-27 17:19:23 +0530

    new live changes

--- src/app/comon/comondata.ts
+++ src/app/comon/comondata.ts
@@ -1,4 +1,4 @@
 export class CommonData {
    // public static baseUrl="http://localhost:3000/";  
-    public static baseUrl="http://ec2-3-137-175-241.us-east-2.compute.amazonaws.com/";
+    public static baseUrl="http://ec2-3-137-175-241.us-east-2.compute.amazonaws.com/nodejs/";
  }
